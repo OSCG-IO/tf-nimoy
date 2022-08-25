174f2c12886e0b967f36c9c047b79cc4811f9a6a
denis lussier <denis@lussier.io>
2022-08-25 16:31:59 -0400

    user_data configuration for driver

--- main.tf
+++ main.tf
@@ -16,47 +16,58 @@ provider "aws" {
 resource "aws_instance" "node1-1" {
   ami           = "ami-0ee02425a4c7e78bb"
   instance_type = "c6g.large"
+  availability_zone = "us-west-2c"
+  key_name = "aws-oregon-key"
   tags = {
     Name = "node1-1"
   }
 }
 
-resource "aws_instance" "node2-1" {
-  ami           = "ami-0ee02425a4c7e78bb"
-  instance_type = "c6g.large"
-  tags = {
-    Name = "node2-1"
-  }
-}
-
-resource "aws_instance" "node3-1" {
-  ami           = "ami-0ee02425a4c7e78bb"
-  instance_type = "c6g.large"
-  tags = {
-    Name = "node3-1"
-  }
-}
 
 resource "aws_instance" "driver1-1" {
   ami           = "ami-0ee02425a4c7e78bb"
   instance_type = "c6g.large"
+  availability_zone = "us-west-2c"
+  key_name = "aws-oregon-key"
   tags = {
     Name = "driver1-1"
   }
+ user_data = <<EOF
+#! /bin/bash
+  sudo apt update -y
+  sudo apt upgrade -y
+  sudo apt install -y wget git openjdk-11-jdk python3 python3-dev python3-pip
+  j_home=/usr/lib/jvm/java-11-openjdk
+  arch=`arch`
+  if [ "$arch" == "aarch64" ]; then
+    j_home=$j_home-arm64
+  fi
+
+  sudo pip3 install git+https://github.com/lilydjwg/pssh
+
+  cd /home/ubuntu
+  rm -rf test
+  mkdir -p test/data
+  cd test
+  git clone https://github.com/OSCG-IO/io
+  git clone https://github.com/OSCG-IO/nimoy
+
+  cd /home/ubuntu
+  ANT=apache-ant-1.9.16-bin
+  rm -f $ANT.tar.gz
+  wget http://mirror.olnevhost.net/pub/apache/ant/binaries/$ANT.tar.gz
+  tar xf $ANT.tar.gz
+  rm $ANT.tar.gz
+
+  echo ""
+  echo "###### adding to /home/ubuntu/.bashrc"
+  echo " "                                       >> /home/ubuntu/.bashrc
+  echo "export JAVA_HOME=$j_home"                >> /home/ubuntu/.bashrc 
+  echo 'export PATH=$PATH:$JAVA_HOME/bin'        >> /home/ubuntu/.bashrc
+  echo 'export ANT_HOME=$HOME/apache-ant-1.9.16' >> /home/ubuntu/.bashrc
+  echo 'export PATH=$ANT_HOME/bin:$PATH'         >> /home/ubuntu/.bashrc
+  echo 'export RMT=$HOME/test/nimoy/remote'      >> /home/ubuntu/.bashrc
+EOF
+
 }
 
-resource "aws_instance" "driver2-1" {
-  ami           = "ami-0ee02425a4c7e78bb"
-  instance_type = "c6g.large"
-  tags = {
-    Name = "driver2-1"
-  }
-}
-
-resource "aws_instance" "driver3-1" {
-  ami           = "ami-0ee02425a4c7e78bb"
-  instance_type = "c6g.large"
-  tags = {
-    Name = "driver3-1"
-  }
-}
