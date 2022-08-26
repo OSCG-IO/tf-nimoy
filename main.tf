789d6d14b2d0c4bff7917dbd3f7c2619f7d5c29e
denis lussier <denis@lussier.io>
2022-08-26 08:09:11 -0400

    round out driver

--- main.tf
+++ main.tf
@@ -34,6 +34,7 @@ resource "aws_instance" "driver1-1" {
   }
  user_data = <<EOF
 #! /bin/bash
+  echo "### Update the OS w/ Java & Python3"
   sudo apt update -y
   sudo apt upgrade -y
   sudo apt install -y wget git openjdk-11-jdk python3 python3-dev python3-pip
@@ -43,15 +44,18 @@ resource "aws_instance" "driver1-1" {
     j_home=$j_home-arm64
   fi
 
+  echo "### Install Paralell SSH"
   sudo pip3 install git+https://github.com/lilydjwg/pssh
 
+  echo "Install NIMOY"
   cd /home/ubuntu
   rm -rf test
   mkdir -p test/data
   cd test
-  git clone https://github.com/OSCG-IO/io
+  ##git clone https://github.com/OSCG-IO/io
   git clone https://github.com/OSCG-IO/nimoy
 
+  echo "Install ANT"
   cd /home/ubuntu
   ANT=apache-ant-1.9.16-bin
   rm -f $ANT.tar.gz
@@ -59,14 +63,28 @@ resource "aws_instance" "driver1-1" {
   tar xf $ANT.tar.gz
   rm $ANT.tar.gz
 
-  echo ""
-  echo "###### adding to /home/ubuntu/.bashrc"
-  echo " "                                       >> /home/ubuntu/.bashrc
+  echo "### Configure .bashrc"
   echo "export JAVA_HOME=$j_home"                >> /home/ubuntu/.bashrc 
   echo 'export PATH=$PATH:$JAVA_HOME/bin'        >> /home/ubuntu/.bashrc
   echo 'export ANT_HOME=$HOME/apache-ant-1.9.16' >> /home/ubuntu/.bashrc
   echo 'export PATH=$ANT_HOME/bin:$PATH'         >> /home/ubuntu/.bashrc
   echo 'export RMT=$HOME/test/nimoy/remote'      >> /home/ubuntu/.bashrc
+
+  echo "### Build nimoy locally"
+  source .bashrc
+  cd /home/ubuntu/test/nimoy/remote
+  ant clean
+  ant
+
+  echo "### Installing IO"
+  cd /home/ubuntu
+  python3 -c "$(curl -fsSL https://oscg-io-download.s3.amazonaws.com/REPO/install.py)"
+  cd oscg
+  ./io install pg14 --start : stop pg14 
+
+  echo "### Generating SSH key"
+  cd /home/ubuntu
+  echo -e "\n\n\n" | ssh-keygen -t rsa
 EOF
 
 }
