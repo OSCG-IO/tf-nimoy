6b55f3b6456a51b5f76b0263e84073c2e6278231
denis lussier <denis@lussier.io>
2022-09-06 17:26:18 -0400

    more of the needful

--- configureServers.sh
+++ configureServers.sh
@@ -6,6 +6,11 @@ set -x
 ## create the 'hosts' & 'ansible_hosts' files from the 'my.out' file
 python3 make_hosts_file.py
 
+## create a new local hosts file with all the new machines
+cp hosts.base.osx hosts.new
+cat hosts >> hosts.new
+sudo cp hosts.new  /etc/hosts
+
 ## driver-1 is the first ip address in the local 'hosts' file
 d1=`head -1 hosts | awk '{print $1;}'`
 key="~/keys/dl-m1book-key.pem"
@@ -23,5 +28,14 @@ ssh -i $key $usr@$d1 'echo -e "\n\n\n" | ssh-keygen -t rsa'
 ssh -i $key $usr@$d1 'ansible-playbook add-key.yml -i ansible_hosts --user ubuntu --key-file ~/keys/dl-m1book-key.pem  -e "key=/home/ubuntu/.ssh/id_rsa.pub"'
 ssh -i $key $usr@$d1 'sudo ./catHosts.sh'
 
-ssh -i $key $usr@$d1 'cd test/nimoy/remote/benchmarksql; /home/ubuntu/apache-ant-1.9.16/bin/ant'
+bs='cd test/nimoy/remote/benchmarksql; /home/ubuntu/apache-ant-1.9.16/bin/ant'
+io='python3 -c "$(curl -fsSL https://oscg-io-download.s3.amazonaws.com/REPO/install.py)"; cd oscg; ./io install pg14; echo "*:5432:*:postgres:password" >> /home/ubuntu/.pgpass; chmod 600 /home/ubuntu/.pgpass'
 
+ssh $usr@driver1-1 "$bs"
+ssh $usr@driver1-1 "$io"
+
+ssh $usr@driver2-1 "$bs"
+ssh $usr@driver2-1 "$io"
+
+ssh $usr@driver3-1 "$bs"
+ssh $usr@driver3-1 "$io"
