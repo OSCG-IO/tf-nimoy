ba0bcf06e1bf48cf1256107f8652107b1ce827b3
denis lussier <denis@lussier.io>
2022-09-07 06:07:22 -0400

    decrease interactive prompts

--- configureServers.sh
+++ configureServers.sh
@@ -10,32 +10,33 @@ python3 make_hosts_file.py
 cp hosts.base.osx hosts.new
 cat hosts >> hosts.new
 sudo cp hosts.new  /etc/hosts
+d1=driver1-1
 
-## driver-1 is the first ip address in the local 'hosts' file
-d1=`head -1 hosts | awk '{print $1;}'`
 key="~/keys/dl-m1book-key.pem"
 usr=ubuntu
+SSH="ssh -o StrictHostKeyChecking=no"
+SCP="scp -o StrictHostKeyChecking=no"
 
-scp -i $key  ansible_hosts  $usr@$d1:.
-scp -i $key  add-key.yml    $usr@$d1:.
-scp -i $key  hosts          $usr@$d1:.
-scp -i $key  catHosts.sh    $usr@$d1:.
+$SCP -i $key  ansible_hosts  $usr@$d1:.
+$SCP -i $key  add-key.yml    $usr@$d1:.
+$SCP -i $key  hosts          $usr@$d1:.
+$SCP -i $key  catHosts.sh    $usr@$d1:.
 
-ssh -i $key $usr@$d1 'mkdir keys'
-scp -i $key ~/keys/dl-m1book-key.pem  $usr@$d1:keys/.
-ssh -i $key $usr@$d1 'echo -e "\n\n\n" | ssh-keygen -t rsa'
+$SSH -i $key $usr@$d1 'mkdir keys'
+$SCP -i $key ~/keys/dl-m1book-key.pem  $usr@$d1:keys/.
+$SSH -i $key $usr@$d1 'echo -e "\n\n\n" | ssh-keygen -t rsa'
 
-ssh -i $key $usr@$d1 'ansible-playbook add-key.yml -i ansible_hosts --user ubuntu --key-file ~/keys/dl-m1book-key.pem  -e "key=/home/ubuntu/.ssh/id_rsa.pub"'
-ssh -i $key $usr@$d1 'sudo ./catHosts.sh'
+$SSH -i $key $usr@$d1 'ansible-playbook add-key.yml -i ansible_hosts --user ubuntu --key-file ~/keys/dl-m1book-key.pem  -e "key=/home/ubuntu/.ssh/id_rsa.pub"'
+$SSH -i $key $usr@$d1 'sudo ./catHosts.sh'
 
 bs='cd test/nimoy/remote/benchmarksql; /home/ubuntu/apache-ant-1.9.16/bin/ant'
 io='python3 -c "$(curl -fsSL https://oscg-io-download.s3.amazonaws.com/REPO/install.py)"; cd oscg; ./io install pg14; echo "*:5432:*:postgres:password" >> /home/ubuntu/.pgpass; chmod 600 /home/ubuntu/.pgpass'
 
-ssh $usr@driver1-1 "$bs"
-ssh $usr@driver1-1 "$io"
+$SSH $usr@driver1-1 "$bs"
+$SSH $usr@driver1-1 "$io"
 
-ssh $usr@driver2-1 "$bs"
-ssh $usr@driver2-1 "$io"
+$SSH $usr@driver2-1 "$bs"
+$SSH $usr@driver2-1 "$io"
 
-ssh $usr@driver3-1 "$bs"
-ssh $usr@driver3-1 "$io"
+$SSH $usr@driver3-1 "$bs"
+$SSH $usr@driver3-1 "$io"
