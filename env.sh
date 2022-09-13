22173d727b46bf3874777e6b5d8bd14fda78e909
denis lussier <denis@lussier.io>
2022-09-13 08:11:21 -0400

    default to 3 nodes of montreal-dublin-sydney

--- env.sh
+++ env.sh
@@ -1,19 +1,19 @@
 
 ## arm64 w/ 1 vcpu, 2 GB Memory
-TYPE=c6g.medium
-N1=oregon
-N2=oregon
-N3=oregon
+#TYPE=c6g.medium
+#N1=oregon
+#N2=oregon
+#N3=oregon
 
 
 ## arm64 w/ 2 vcpu, 4 GB Memory
-#TYPE=c6g.large 
+TYPE=c6g.large 
 
 ## now:    oregon, montreal, dublin, sydney
 ## soon:   dallas, osaka
-#N1=montreal
-#N2=dublin
-#N3=sydney
+N1=montreal
+N2=dublin
+N3=sydney
 
 creds="./aws-creds.sh"
 if [ -f "$creds" ]; then
