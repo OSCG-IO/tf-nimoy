9bea1750feb40d6cbae322cc4c35a5f7cc3f27c9
Cady Motyka <cadymo@Cadys-MacBook-Pro.local>
2022-09-15 11:42:46 -0400

    First pass as sg in tf

--- outputs.tf
+++ outputs.tf
@@ -3,3 +3,5 @@ output "driver_private_ip" { value = aws_instance.driver.private_ip }
 
 output "node_public_ip"    { value  = aws_instance.node.public_ip }
 output "node_private_ip"   { value  = aws_instance.node.private_ip }
+
+output "sg_id" { value = aws_security_group.sg.id }
