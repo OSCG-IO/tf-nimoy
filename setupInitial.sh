525cb5e87f33d9f20178b812159c85579f491848
denis lussier <denis@lussier.io>
2022-09-16 03:11:48 +0000

    support Linux as the launcher (as well as OSX)

---
+++ setupInitial.sh
@@ -0,0 +1,22 @@
+
+set -x
+
+os=`uname -s`
+if [ "$os" == "Darwin" ]; then
+  brew install terraform
+  brew install ansible
+  exit $?
+else
+  yum --version > /dev/null 2>&1
+  rc=$?
+  if [ ! "$rc" == "0" ]; then
+    echo "Only EL8+ supported"
+    exit 1
+  fi
+fi
+
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+sudo yum install -y terraform
+sudo dnf install -y epel-release
+sudo dnf install -y ansible
