a09b5d02a47ea26de4b4c06f2edef20f586645b3
denis lussier <denis@lussier.io>
2022-10-10 09:19:31 +0000

    start prometheus exporter by default

--- remote/setupSpock.sh
+++ remote/setupSpock.sh
@@ -11,6 +11,6 @@ set -x
 
 $SSH $HOST "$io install $pgXX"
 $SCP ~/.pgpass $HOST:$PG/.pgpass
-$SSH $HOST "$io start $pgXX -y -d demo : tune $pgXX : install spock -d demo : install prompgexp"
+$SSH $HOST "$io start $pgXX -y -d demo : tune $pgXX : install spock -d demo : install prompgexp --start"
 
 $SCP pg_hba.conf $HOST:$PG/.
