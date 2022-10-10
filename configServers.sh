18493ae986565d6384048742bcf177150742ff90
denis lussier <denis@lussier.io>
2022-10-10 06:39:55 +0000

    improve configServers to run from any host

--- configServers.sh
+++ configServers.sh
@@ -16,12 +16,12 @@ d1=driver1-1
 
 key="keys/dl-m1book-key.pem"
 usr=ubuntu
-SSH="ssh -o StrictHostKeyChecking=no"
-SCP="scp -o StrictHostKeyChecking=no"
+SSH="ssh -i $key -o StrictHostKeyChecking=no"
+SCP="scp -i $key -o StrictHostKeyChecking=no"
 PASS=$(openssl rand -base64 12;)
 
-$SCP -i $key  ansible_hosts  $usr@$d1:.
-$SCP -i $key  add-key.yml    $usr@$d1:.
+$SCP ansible_hosts  $usr@$d1:.
+$SCP add-key.yml    $usr@$d1:.
 
 setupEtcHosts driver1-1
 setupEtcHosts node1-1
@@ -30,12 +30,12 @@ setupEtcHosts node2-1
 setupEtcHosts driver3-1
 setupEtcHosts node3-1
 
-$SSH -i $key $usr@$d1 'mkdir keys'
-$SCP -i $key $key  $usr@$d1:keys/.
-$SSH -i $key $usr@$d1 'echo -e "\n\n\n" | ssh-keygen -t rsa'
+$SSH $usr@$d1 'mkdir keys'
+$SCP $usr@$d1:keys/.
+$SSH $usr@$d1 'echo -e "\n\n\n" | ssh-keygen -t rsa'
 
-$SSH -i $key $usr@$d1 'ansible-playbook add-key.yml -i ansible_hosts --user ubuntu --key-file $key  -e "key=/home/ubuntu/.ssh/id_rsa.pub"'
-$SSH -i $key $usr@$d1 'sudo ./catHosts.sh'
+$SSH $usr@$d1 'ansible-playbook add-key.yml -i ansible_hosts --user ubuntu --key-file $key  -e "key=/home/ubuntu/.ssh/id_rsa.pub"'
+$SSH $usr@$d1 'sudo ./catHosts.sh'
 
 bs='cd test/tf-nimoy/remote/benchmarksql; /home/ubuntu/apache-ant-1.9.16/bin/ant'
 io='python3 -c "$(curl -fsSL https://oscg-io-download.s3.amazonaws.com/REPO/install.py)"; cd oscg; ./io install pg'
