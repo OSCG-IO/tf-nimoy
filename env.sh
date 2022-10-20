bd60409a082e04362220c11b37903e1db420c5b2
Cady Motyka <cadymo@Cadys-MacBook-Pro.local>
2022-10-20 09:35:17 -0400

    adding dev branch

--- env.sh
+++ env.sh
@@ -1,23 +1,23 @@
 
-CLUSTER_NM=domo
+CLUSTER_NM=demo
 CLOUD=aws
 TYPE=c6g.medium
 PGV=15
 NODE_KOUNT=3
 
-#N1=pdt
-#N1Z=a
-#N2=pdt
-#N2Z=b
-#N3=pdt
-#N3Z=c
-
-N1=mtl
+N1=pdt
 N1Z=a
-N2=dub
-N2Z=a
-N3=syd
-N3Z=a
+N2=pdt
+N2Z=b
+N3=pdt
+N3Z=c
+
+#N1=mtl
+#N1Z=a
+#N2=dub
+#N2Z=a
+#N3=syd
+#N3Z=a
 
 creds="keys/aws-creds.sh"
 if [ -f "$creds" ]; then
