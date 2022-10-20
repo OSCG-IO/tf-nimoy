03a7e683c0314470f65afb71faebfff6551e901c
Cady Motyka <cadymo@Cadys-MacBook-Pro.local>
2022-10-20 14:50:57 -0400

    sq rule set wip

--- scripts/make_sg_rules.py
+++ scripts/make_sg_rules.py
@@ -1,12 +1,12 @@
-import boto3
-import os
+import boto3,sys,os
+from botocore.exceptions import ClientError
 
-os.chdir(sys.path[0])
 f = open('my.out', 'r')
 Lines = f.readlines()
+cidr=[]
 nn=""
-cidr=""
-ipPerm=""i
+sgid=[]
+ipval=""
 
 ec2 = boto3.client('ec2')
 
@@ -23,26 +23,35 @@ for line in Lines:
   if l.startswith('node_public_ip'):
     for i in ls:
       if i.startswith('"') and i.endswith('"'):
-        ii = i.replace('"',"'")
-        cidr=cidr+ii + ","
+        ii = i.replace('"',"")
+        cidr.append(ii)
         break
   if l.startswith('driver_public_ip'):
     for i in ls:
       if i.startswith('"') and i.endswith('"'):
-        ii = i.replace('"',"'")
-        cidr=cidr+ii + ","  
+        ii = i.replace('"',"")
+        cidr.append(ii)  
         break
+  if l.startswith('sg_id'):
+    for i in ls:
+      if i.startswith('"') and i.endswith('"'):
+        ii = i.replace('"',"")
+        sgid.append(ii)
 
-cidr=cidr[:-1]
-ipPerm="""IpPermissions=[
+for x in sgid:
+  for y in cidr:
+    try:
+      ipval=y+"/32"
+      data = ec2.authorize_security_group_ingress( GroupId= x ,IpPermissions= [
+            {'IpProtocol': 'tcp',
+             'FromPort': 80,
+             'ToPort': 80,
+             'IpRanges': [{'CidrIp': ipval }]},
             {'IpProtocol': 'tcp',
              'FromPort': 22,
              'ToPort': 22,
-             'IpRanges': [{'CidrIp': """ + cidr + """}]},
-            {'IpProtocol': 'tcp',
-             'FromPort': 5432,
-             'ToPort': 5432,
-             'IpRanges': [{'CidrIp': """ + cidr + """}]}
-        ]"""
-
-print('Ingress Successfully Set %s' % ipPerm)
+             'IpRanges': [{'CidrIp': ipval}]}
+        ])
+      print('Ingress rule set: %s' % data)
+    except ClientError as e:
+      print(e)
