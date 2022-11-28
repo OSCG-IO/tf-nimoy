6ac441141f37571f34b29e01ee75d48580d0a207
denis lussier <denis@lussier.io>
2022-11-28 20:27:06 +0000

    fix arm64 el8 install

--- setupInitial.sh
+++ setupInitial.sh
@@ -11,14 +11,25 @@ else
   yum --version > /dev/null 2>&1
   rc=$?
   if [ ! "$rc" == "0" ]; then
-    echo "Only EL8+ supported"
+    echo "Only EL8+ ARM64 supported"
+    exit 1
+  fi
+  if [ ! `arch` == "aarch64" ]; then
+    echo "Only EL8+ ARM64 supported"
     exit 1
   fi
 fi
 
-sudo yum install -y yum-utils
-sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
-sudo yum install -y terraform
+sudo dnf install -y wget zip
+
+rm terraform*
+zip_file=terraform_1.3.5_linux_arm64.zip
+wget https://releases.hashicorp.com/terraform/1.3.5/$zip_file
+unzip $zip_file
+sudo mv terraform /usr/local/bin/.
+sudo chmod 755 /usr/local/bin/terraform 
+rm $zip_file
+
 sudo dnf install -y epel-release
 sudo dnf install -y ansible
 sudo dnf install -y pv
