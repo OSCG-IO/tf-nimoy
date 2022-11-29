fad8bbc5e1ef215f027775d90cc8f446107bd111
Cloud User <clmotyka@gmail.com>
2022-11-29 18:34:11 +0000

    Finished- ansible changes pre db

--- scripts/configServers.sh
+++ scripts/configServers.sh
@@ -4,6 +4,7 @@ cd ..
 
 cluster=$1
 clDir=$PWD/nodes/$cluster
+rm $clDir/.pgpass
 
 if [ ! -f "$clDir/env.sh" ]; then
   echo "FATAL ERROR: missing env.sh file"
@@ -19,6 +20,7 @@ PASS=$(openssl rand -hex 8;)
 echo "localhost:5432:*:postgres:$PASS" >> $clDir/.pgpass
 echo "127.0.0.1:5432:*:postgres:$PASS" >> $clDir/.pgpass
 input="$clDir/ansible_hosts_node"
+item=1
 while IFS= read -r line
 do
    if [[ $line == "[hosts]" ]]; then
@@ -26,35 +28,39 @@ do
    elif [[ $line == "" ]]; then
       break
    else
+      echo "node${item}-1:5432:*:postgres:$PASS" >> $clDir/.pgpass
       echo "$line:5432:*:postgres:$PASS" >> $clDir/.pgpass
+      item=$((item+1))
    fi
 done < "$input"
 
-echo "using key: $key"
 echo "Configuring Nodes"
 
 ## Cat hosts to /etc/hosts on each VM
-ansible-playbook -i $clDir/ansible_hosts_node --user $usr --key-file $key -e \"key=/home/centos/.ssh/id_rsa.pub\" ansible/cat-hosts.yml
+ansible-playbook -i $clDir/ansible_hosts_node --user $usr --key-file $key ansible/cat-hosts.yml
 
 ## Add keys to each VM
-ansible-playbook -i $clDir/ansible_hosts_node --user $usr --key-file $key -e \"key=/home/centos/.ssh/id_rsa.pub\" ansible/add-key.yml
+ansible-playbook -i $clDir/ansible_hosts_node --user $usr --key-file $key -e "KEYFILE=../${key/.pem/.pub}" ansible/add-key.yml
 
 ## Run io install, io start
 ansible-playbook -i $clDir/ansible_hosts_node --user $usr --key-file $key -e "PGV=$PGV PASS=$PASS PGFile=$clDir/.pgpass" ansible/io-install.yml
 
 if [[ $DEMO=="True" ]]; then
   echo "Configuring Drivers"
-  d1=driver1-1
-  SCP="scp -i $key -o StrictHostKeyChecking=no"
-  $SCP $clDir/demo.sql $usr@$d1:.
 
   ## Cat hosts to /etc/hosts on each VM
-  ansible-playbook -i $clDir/ansible_hosts_driver --user $usr --key-file $key -e \"key=/home/centos/.ssh/id_rsa.pub\" ansible/cat-hosts.yml
+  ansible-playbook -i $clDir/ansible_hosts_driver --user $usr --key-file $key ansible/cat-hosts.yml
 
   ## Add keys to each VM
-  ansible-playbook -i $clDir/ansible_hosts_driver --user $usr --key-file $key -e \"key=/home/centos/.ssh/id_rsa.pub\" ansible/add-key.yml
+  ansible-playbook -i $clDir/ansible_hosts_driver --user $usr --key-file $key -e "KEYFILE=../${key/.pem/.pub}" ansible/add-key.yml
 
   ## Run io install, build demo, and set up password file
-  ansible-playbook -i $clDir/ansible_hosts_driver --user $usr --key-file $key -e "PGV=$PGV PASS=$PASS PGFile=$clDir/.pgpass" ansible/demo-install.yml
+  ansible-playbook -i $clDir/ansible_hosts_driver --user $usr --key-file $key -e "PGV=$PGV PASS=$PASS PGFile=$clDir/.pgpass DEMSQL=$clDir/demo.sql" ansible/demo-install.yml
+  rm $clDir/demo.sql
 fi
+
+##rm $clDir/.pgpass
+##rm $clDir/ansible_hosts*
+
+echo "Database Cluster Ready"
 
