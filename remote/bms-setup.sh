77847eaa383dc406a332159b1c2a56ce587dacbb
Cloud User <clmotyka@gmail.com>
2022-12-01 20:52:36 +0000

    Key fix and duh on regionalization

--- remote/bms-setup.sh
+++ remote/bms-setup.sh
@@ -2,15 +2,18 @@
 source env.sh
 
 ./bms-restore.sh node1-1
-psql -h node1-1 -U postgres demo -f demo-regonalize.sql --variable=REGION=1 
+psql -h node1-1 -U postgres demo -f demo-regionalize.sql --variable=REGION=2 
+psql -h node1-1 -U postgres demo -f demo-regionalize.sql --variable=REGION=3
 ./bms-spock-create-node.sh node1-1
 
 ./bms-restore.sh node2-1
-psql -h node2-1 -U postgres demo -f demo-regonalize.sql --variable=REGION=2
+psql -h node2-1 -U postgres demo -f demo-regionalize.sql --variable=REGION=1
+psql -h node2-1 -U postgres demo -f demo-regionalize.sql --variable=REGION=3
 ./bms-spock-create-node.sh node2-1
 
 ./bms-restore.sh node3-1
-psql -h node3-1 -U postgres demo -f demo-regonalize.sql --variable=REGION=3
+psql -h node3-1 -U postgres demo -f demo-regionalize.sql --variable=REGION=1
+psql -h node3-1 -U postgres demo -f demo-regionalize.sql --variable=REGION=2
 ./bms-spock-create-node.sh node3-1 
 
 SCP="scp -o StrictHostKeyChecking=accept-new"
